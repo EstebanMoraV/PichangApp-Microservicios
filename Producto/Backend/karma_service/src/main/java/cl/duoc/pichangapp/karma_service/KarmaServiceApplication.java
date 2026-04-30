79ba8740c7bc6ecf3ecdcd3582b02048f8d1409b
Esteban <esteban.morauc@gmail.com>
2026-04-30 17:05:23 -0400

    Se crea un nuevo microservicio de karma, tambien se hace creación de dos nuevas ramas, de desarrollo y de testing.

---
+++ Producto/Backend/karma_service/src/main/java/cl/duoc/pichangapp/karma_service/KarmaServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.pichangapp.karma_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmaServiceApplication.class, args);
+	}
+
+}
